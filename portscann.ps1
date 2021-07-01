7256747a33312a8183a47b2e1c544e49b3efc689
dom1nick <60849061+gabri-silva@users.noreply.github.com>
2021-07-01 15:38:15 -0300

    Add files via upload

---
+++ portscann.ps1
@@ -0,0 +1,14 @@
+﻿param($ip)
+if(!$ip){
+   echo "uso: .\script.ps1 host"
+}else{
+$topports = 21,22,25,80,443,3306,8080
+try {foreach ($p in $topports){
+    if (Test-NetConnection $ip -Port $p -WarningAction SilentlyContinue -InformationLevel Quiet) {
+        echo "OPEN:  $($p)"
+    }}else {
+        echo "CLOSE: $($p)"
+    }
+
+} catch{}
+}
